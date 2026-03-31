4a04c02519663f676254f52c36e8e92587b43c90
Emmanuel Marin <emmagodredmi@gmail.com>
2026-03-31 14:26:34 -0400

    feat: Estrusctura inicial del proyecto

---
+++ Enmanuel/app.py
@@ -0,0 +1,57 @@
+from funciones import (
+    cargar_inventario,
+    registrar_producto,
+    listar_productos,
+    buscar_producto,
+    actualizar_producto,
+    eliminar_producto,
+)
+
+
+def mostrar_menu():
+
+    print(" [1]  Registrar producto                 ")
+    print(" [2]  Listar productos                   ")
+    print(" [3]  Buscar producto                    ")
+    print(" [4]  Actualizar producto                ")
+    print(" [5]  Eliminar producto                  ")
+    print(" [6]  Salir                              ")
+
+
+def main():
+
+    print("Bienvenido al Sistema de Inventario ")
+    print("Cargando datos...")
+
+    inventario = cargar_inventario()
+    print(f"{len(inventario)} producto(s) cargado(s) desde inventario.json")
+
+    while True:
+        mostrar_menu()
+        opcion = input("Elige una opción (1-6): ").strip()
+
+        if opcion == "1":
+            registrar_producto(inventario)
+
+        elif opcion == "2":
+            listar_productos(inventario)
+
+        elif opcion == "3":
+            buscar_producto(inventario)
+
+        elif opcion == "4":
+            actualizar_producto(inventario)
+
+        elif opcion == "5":
+            eliminar_producto(inventario)
+
+        elif opcion == "6":
+            print("\n ¡Hasta pronto! Los datos han sido guardados.")
+            break
+
+        else:
+            print("Opción no válida. Por favor elige un número del 1 al 6.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Enmanuel/funciones.py
@@ -0,0 +1,156 @@
+import json
+import os  # importamos os porque nos ayuda a buscar archivos y carpetas en nuestra computadora
+
+
+RUTA_DATOS = os.path.join(os.path.dirname(__file__), "data", "inventario.json")
+# __file__ es donde esta el archivo app.py
+# dirname es la carpeta donde esta el archivo app.py
+# join es para unir las carpetas
+# data es la carpeta donde esta el archivo inventario.json
+
+
+def cargar_inventario():
+    # muestra los datos del archivo json
+    # primero vemos si el archivo existe
+    if os.path.exists(RUTA_DATOS):
+        # abrimos el archivo en modo lectura
+        with open(RUTA_DATOS, "r", encoding="utf-8") as archivo:
+            contenido = archivo.read()  # leemos todo el texto
+            if contenido.strip() == "":  # si esta vacio o en blanco
+                return []  # devolvemos una lista vacia
+            return json.loads(
+                contenido
+            )  # se utiliza para convertir una cadena de texto a un objeto de Python
+
+    return (
+        []
+    )  # si el archivo no existe o no hay datos entonces empezamos con una lista vacia
+
+
+def guardar_inventario(inventario):
+    # esta funcion toma nuestro inventario y lo guarda en el archivo
+    with open(RUTA_DATOS, "w", encoding="utf-8") as archivo:
+        # "W" escribir dentro del archivo
+        json.dump(inventario, archivo, ensure_ascii=False)
+        # ensure_ascii=False es para que el nombre del producto no salga asi EJ: "café" y no "caf\u00e9"
+
+
+def generar_codigo(inventario):
+    # esta funcion crea un codigo unico para cada producto nuevo EJ: PROD-005
+
+    # si el inventario esta vacio, sera nuestro primer producto: PROD-001
+    if len(inventario) == 0:
+        return "PROD-001"
+
+    # aqui buscamos cual es el numero mas grande que ya hemos usado en los codigos
+    numero_mayor = 0
+    for producto in inventario:
+        # tomamos el ID del producto actual EJ: PROD-003
+        ID = producto.get("ID", "PROD-000")
+        try:
+            # cortamos el texto por el guion "-" y nos quedamos con el numero: "003" y lo volvemos numero
+            numero = int(ID.split("-")[1])
+            if numero > numero_mayor:
+                numero_mayor = numero  # actualizamos el numero mas grande guardado
+        except:
+            pass  # si pasa algun error raro con un codigo, lo ignoramos y seguimos
+
+    # el nuevo numero sera el mayor de los que teniamos, mas 1
+    nuevo_numero = numero_mayor + 1
+    # juntamos la palabra "PROD-" y el numero nuevo con ceros a la izquierda 004, 015, etc
+    return f"PROD-{nuevo_numero:03d}"
+
+
+def buscar_por_nombre_o_codigo(inventario, termino):
+    # esta funcion sirve para buscar un producto en nuestro inventario
+    # puede ser buscando por su nombre "manzana" o por su codigo "PROD-002"
+
+    termino = (
+        termino.strip().upper()
+    )  # convertimos la busqueda en mayusculas para que sea facil comparar
+    for producto in inventario:
+        nombre_upper = producto[
+            "nombre"
+        ].upper()  # el nombre del producto en mayusculas
+        ID_upper = producto["ID"].upper()  # el codigo del producto en mayusculas
+
+        # si la palabra que buscamos esta dentro del nombre, o es exactamente igual al codigo...
+        if termino in nombre_upper or termino == ID_upper:
+            return producto  # encontramos el producto, lo devolvemos para poder usarlo
+
+    return None  # si revisamos toda la lista y no encontramos nada, retornamos none
+
+
+def registrar_producto(inventario):
+    # regristra un nuevo producto en el inventario
+    print("\n REGISTRAR NUEVO PRODUCTO ")
+
+    # pedimos el nombre y la categoria
+    nombre = input("  Nombre del producto : ").strip()
+    categoria = input("  Categoría        : ").strip()
+
+    # pedimos el precio y la cantidad en stock
+    try:
+        precio = float(
+            input("  Precio ($)          : ")
+        )  # float para precio con decimales
+        cantidad = int(input("  Cantidad en stock   : "))  # int para cantidad entera
+    except ValueError:
+        # si el usuario escribio letras en la pregunta del precio, le decimos que hay un error
+        print("Error: el precio y la cantidad deben ser numeros validos.")
+        return  # cancelamos la tarea
+
+    # usamos nuestra otra funcion de buscar para asegurarnos de no tener productos con el mismo nombre repetido
+    if buscar_por_nombre_o_codigo(inventario, nombre):
+        print(f" Ya existe un producto con el nombre '{nombre}'.")
+        return  # cancelamos para no tener prodcutos repetidos
+
+    # generar ID para el nuevo producto
+    ID = generar_codigo(inventario)
+
+    # creamos un diccionario donde adentro ponemos toda la informacion del producto
+    nuevo_producto = {
+        "ID": ID,
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad,
+        "categoria": categoria,
+    }
+
+    # .append para agregar nuestro nuevo producto al inventario
+    inventario.append(nuevo_producto)
+
+    # guardamos los cambios en el archivo
+    guardar_inventario(inventario)
+    print(f"Producto '{nombre}' registrado con ID {ID}.")
+
+
+def listar_productos(inventario):
+    # esta funcion nos muestra todos los productos que tenemos en el inventario
+    print("\n INVENTARIO COMPLETO")
+
+    # si revisamos la lista y no hay ningun producto, entonces mostramos un mensaje
+    if len(inventario) == 0:
+        print("No hay productos registrados aun.")
+        return
+
+    # coloco una linea separadora bonita repitiendo "─" 68 veces para el encabezado de los productos
+    # vamos leyendo producto por producto para colocarlo
+    print(
+        f"\n  {'#':<4} {'Código':<10} {'Nombre':<20} {'Precio':>10} {'Stock':>6} {'Categoría':<15}"
+    )
+    print("  " + "─" * 68)
+
+    # enumerate nos ayuda a llevar un conteo facil de que numero de lista es (1, 2, 3...etc)
+    for numero, producto in enumerate(inventario, start=1):
+        # acomodamos toda la información de nuestro diccionario adentro del texto, usando < > para alinear
+        print(
+            f"  {numero:<4} "
+            f"{producto['ID']:<10} "
+            f"{producto['nombre']:<20} "
+            f"${producto['precio']:>9.2f} "  # .2f significa que muestra solo 2 numeros decimales del precio
+            f"{producto['cantidad']:>6} "
+            f"{producto['categoria']:<15}"
+        )
+
+    print(f"\n Total de productos: {len(inventario)}")
